c0867fb504f6c107789b4214d4fcf494d647d5b1
Jing_Xu <jing_xu@yeah.net>
2019-08-30 10:45:56 +0800

    第二次上传，用numpy数组传输数据

--- numpy_tutorial/src/numpy_listener.py
+++ numpy_tutorial/src/numpy_listener.py
@@ -4,13 +4,14 @@ import roslib; roslib.load_manifest(PKG)
 
 import rospy
 from rospy_tutorials.msg import Floats
+from rospy.numpy_msg import numpy_msg
 
 def callback(data):
     print rospy.get_name(), "I heard %s"%str(data.data)
 
 def listener():
     rospy.init_node('listener')
-    rospy.Subscriber("floats", Floats, callback)
+    rospy.Subscriber("floats", numpy_msg(Floats), callback)
     rospy.spin()
 
 if __name__ == '__main__':
